69f23d8ae7135c9c2ee74a55cde75698ed6a984e
Akshat Singhal <u6322388@anu.edu.au>
2018-09-21 13:48:33 +1000

    3

--- src/ShoeSize2.java
+++ src/ShoeSize2.java
@@ -1,19 +1,14 @@
 /* ShoeSize - Eric McCreath 2015 - GPL
  * This class stores a persons shoe size.
  */
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 
 
 public class ShoeSize2 {
     private static final String SHOESIZEENAME = "SHOESIZE";
     public static final int SHOESIZEMAX = 15;
     public static final int SHOESIZEMIN = 3;
-    private static final String FILENAME = "data";
+    private static final String FILENAME = "data2";
 
     private Integer shoesize;
 
@@ -36,35 +31,36 @@ public class ShoeSize2 {
         }
     }
 
-    static ShoeSize load() {
-        ShoeSize shoeSize = new ShoeSize();
+    static ShoeSize2 load() {
+        ShoeSize2 shoeSize = new ShoeSize2();
         // add code here that will load shoe size from a file called "FILENAME"
-        try{
-            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILENAME));
+        if((new File(FILENAME)).exists()) {
+            try {
+                ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILENAME));
 
-            shoeSize.set((Integer) ois.readObject());
-            ois.close();
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-        catch (ClassNotFoundException e){
-            e.printStackTrace();
+                shoeSize.set((Integer) ois.readObject());
+                ois.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
         }
         // return new ShoeSize();
         return shoeSize;
     }
 
     void save() {
-        try{
-            ObjectOutputStream oos = new ObjectOutputStream(
-                    new FileOutputStream(FILENAME));
-            oos.writeObject(this.shoesize);
-            oos.close();
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
+
+            try {
+                ObjectOutputStream oos = new ObjectOutputStream(
+                        new FileOutputStream(FILENAME));
+                oos.writeObject(this.shoesize);
+                oos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
 
         // add code here that will save shoe size into a file called "FILENAME"
     }
